72b355c596f9008525a521fc12c12b7e06675203
Ersocaut <leonardo.soy.yo@hotmail.com>
2020-10-05 10:58:45 -0500

    Tests

--- CVDS-LAB08/src/test/java/edu/eci/cvds/test/ServiciosAlquilerTest.java
+++ CVDS-LAB08/src/test/java/edu/eci/cvds/test/ServiciosAlquilerTest.java
@@ -1,11 +1,13 @@
 package edu.eci.cvds.test;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.google.inject.Inject;
 import edu.eci.cvds.sampleprj.dao.PersistenceException;
 import edu.eci.cvds.samples.entities.Cliente;
+import edu.eci.cvds.samples.entities.Item;
 import edu.eci.cvds.samples.entities.ItemRentado;
 import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquiler;
@@ -54,8 +56,40 @@ public class ServiciosAlquilerTest {
             Cliente cliente = serviciosAlquiler.consultarCliente(2160666);
             Assert.assertEquals("Javier López",cliente.getNombre());
         } catch(ExcepcionServiciosAlquiler e) {
-            System.out.println("JAJAJSJJAJAJ pelotudo");
             throw new ExcepcionServiciosAlquiler("Error Prueba Consultar Cliente.",e);
         }
     }
+
+    @Test
+    public void itemNoFound(){
+        boolean found = false;
+        try{
+            Item item = serviciosAlquiler.consultarItem(526341);
+        }
+        catch (ExcepcionServiciosAlquiler e){
+            found = true;
+        }
+        catch (IndexOutOfBoundsException e){
+            found = true;
+        }
+        finally{
+            Assert.assertTrue(found);
+        }
+    }
+
+    @Test
+    public void insertarItemRentado() throws ExcepcionServiciosAlquiler {
+        boolean insert = true;
+        try{
+            Item item = serviciosAlquiler.consultarItem(102);
+            serviciosAlquiler.registrarAlquilerCliente(Date.valueOf("2019-02-01"),999,item,5);
+        }
+        catch(ExcepcionServiciosAlquiler e){
+            insert = false;
+        }
+        finally{
+            Assert.assertTrue(insert);
+        }
+    }
+
 }
